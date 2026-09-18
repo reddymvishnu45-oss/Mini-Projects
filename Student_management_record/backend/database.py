255435601584158f01ecab731328843d1e76a90a
reddymvishnu45-oss <reddymvishnu45@gmail.com>
2026-09-19 00:43:54 +0530

    completed

--- Student_management_record/backend/database.py
+++ Student_management_record/backend/database.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print(DATABASE_URL)
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set")
