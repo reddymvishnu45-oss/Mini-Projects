79eacaf98321f8d2146f3aa72889cbc2ee1163ac
reddymvishnu45-oss <reddymvishnu45@gmail.com>
2026-09-19 00:52:33 +0530

    completed

--- Student_management_record/backend/database.py
+++ Student_management_record/backend/database.py
@@ -1,29 +1,26 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from urllib.parse import quote_plus
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-print(DATABASE_URL)
+DB_USER = os.getenv("POSTGRES_USER", "postgres")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_HOST = os.getenv("POSTGRES_HOST", "db")
+DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+DB_NAME = os.getenv("POSTGRES_DB", "myapp")
 
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL environment variable is not set")
+DB_PASSWORD = quote_plus(DB_PASSWORD)
 
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True
+DATABASE_URL = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
-
-SessionLocal = sessionmaker(
-    bind=engine,
-    autoflush=False,
-    autocommit=False
-)
-
+engine = create_engine(DATABASE_URL,pool_pre_ping = True)
+SessionLocal = sessionmaker(bind=engine,autoflush=True,autocommit= False)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
